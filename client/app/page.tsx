ad90e42a561decd04a8a6f143b82fd95fd06ceec
HoriaMitrica <horia.mitrica@gmail.com>
2024-07-12 11:24:43 +0300

    imported svg refs and created logo component

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import MyImage from '@/public/clean_code_text.svg';
+import MyLogo from '@/public/logo_color.svg';
 import styles from '@/app/page.module.css';
 
 export default function Home() {
   return (
     <>  
-      <MyImage className={styles.svg} />
+      <MyLogo className={styles.svg}/>
     </>
   );
 };
